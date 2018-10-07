1661d27116b86054641e3ca88529f5b21416a9bd
Avi K <kushagrakhare@rocketmail.com>
2018-10-07 14:39:04 +0530

    Added Auto Focus in AR Camera

---
+++ Assets/Scripts/CameraFocusController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour {
+
+	void Start() {    
+		var vuforia = VuforiaARController.Instance;    
+		vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);    
+		vuforia.RegisterOnPauseCallback(OnPaused);
+	}  
+
+	private void OnVuforiaStarted() {    
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnPaused(bool paused) {    
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);    
+		}
+	}
+}
