1a7544b7aa415eab4643d6d64a22c18f3f4f018a
Shreyas Iyer <shreyasiyer14@gmail.com>
2018-10-11 17:10:32 +0530

    Added Iron man model with basic controls

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 15f;
+
+	public void TranslateX(int direction) {
+		if (direction == 1)
+			transform.rotation = Quaternion.Euler (Vector3.zero);
+		else
+			transform.rotation = Quaternion.Euler (transform.up * 180f);
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	public void TranslateY(int direction) {
+		transform.rotation = Quaternion.Euler (transform.up * direction * 90f);
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	public Controller playerController;
+	public int xDir = 0;
+	public int yDir = 0;
+
+	bool _pressed = false;
+	public void OnPointerDown(PointerEventData eventData) {
+		_pressed = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData) {
+		_pressed = false;
+	}
+
+	void Update() {
+		if (!_pressed)
+			return;
+		if (Mathf.Abs (xDir) == 0) {
+			playerController.TranslateY (yDir);
+		} else {
+			playerController.TranslateX (xDir);
+		}
+	}
+}
